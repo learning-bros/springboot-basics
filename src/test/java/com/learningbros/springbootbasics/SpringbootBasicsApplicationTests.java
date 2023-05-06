e5dca40a34a4e2f36b8d52339a55fdac7a8f73e8
Raul Sempere Trujillo <raulst.prog@gmail.com>
2023-05-06 13:11:15 +0200

    first commit

---
+++ src/test/java/com/learningbros/springbootbasics/SpringbootBasicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learningbros.springbootbasics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBasicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
